3451eae3cd07ce5fc804e89d757492b096355eeb
jandziatkowski <126346520+jandziatkowski@users.noreply.github.com>
2023-04-14 23:28:00 +0200

    Add files via upload

--- assets/js/copytext.js
+++ assets/js/copytext.js
@@ -1,11 +1,15 @@
-let copyText = document.querySelector(".copy-text");
-copyText.querySelector("button").addEventListener("click", function(){
-    let input = copyText.querySelector("input.text");
-    input.select();
-    document.execCommand("copy");
-    copyText.classList.add("active");
-    window.getSelection().removeAllRanges();
-    setTimeout(function(){
-        copyText.classList.remove("active");
-    },2500);
+const copyButton = document.querySelector(".copy-button");
+const popup = document.querySelector(".popup-message"); // get popup message element
+
+copyButton.addEventListener("click", function() {
+  let input = document.querySelector("input.text");
+  input.select();
+  document.execCommand("copy");
+  copyButton.classList.add("active");
+  popup.classList.add("active"); // show popup message
+  window.getSelection().removeAllRanges();
+  setTimeout(function(){
+    copyButton.classList.remove("active");
+    popup.classList.remove("active"); // hide popup message after 2.5 seconds
+  },2500);
 });
---
+++ assets/js/slick-init.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('.work-images').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 1500,
+      arrows: false,
+      dots: false,
+      centerMode: false,
+      centerPadding: '10%',
+      pauseOnHover: false
+    });
+  });
